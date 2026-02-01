f28db1b059e1d24a2760de5a6374c3942ec15923
Sahil Muthoo <sahil.muthoo@gmail.com>
2026-02-01 22:10:28 +0700

    make code a little bit more like the book

--- raytracing.rb
+++ raytracing.rb
@@ -6,12 +6,12 @@ require_relative './vec3'
 require_relative './ray'
 
 aspect_ratio = 16.0 / 9.0
-image_width = 1600
+image_width = 400
 image_height = (image_width / aspect_ratio).to_i
 
 focal_length = 1.0
 viewport_height = 2.0
-viewport_width = viewport_height * (image_width / image_height).to_f
+viewport_width = viewport_height * (image_width.to_f / image_height)
 camera_center = Point3.new(0, 0, 0)
 
 viewport_u = Vec3.new(viewport_width, 0, 0)
@@ -21,7 +21,7 @@ pixel_delta_u = viewport_u / image_width
 pixel_delta_v = viewport_v / image_height
 
 viewport_upper_left = camera_center - Vec3.new(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2
-pixel00_loc = viewport_upper_left + ((pixel_delta_u + pixel_delta_v) / 2)
+pixel00_loc = viewport_upper_left + ((pixel_delta_u + pixel_delta_v) * 0.5)
 
 puts "P3\n#{image_width} #{image_height}\n255"
 
@@ -29,11 +29,6 @@ puts "P3\n#{image_width} #{image_height}\n255"
   STDERR.print("\rScanlines remaining: #{image_height - j}")
   STDERR.flush
   (0...image_width).each do |i|
-    # color = Color.new(
-    #   i.to_f / (image_width - 1),
-    #   j.to_f / (image_height - 1),
-    #   0.0
-    # )
     pixel_center = pixel00_loc + (pixel_delta_u * i) + (pixel_delta_v * j)
     ray_direction = pixel_center - camera_center
     ray = Ray.new(camera_center, ray_direction)
