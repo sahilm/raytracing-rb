472eb509d06a0985385ec3b54ba5fbbe4549e5a0
Sahil Muthoo <sahil.muthoo@gmail.com>
2026-02-03 15:34:01 +0700

    refactor 2

--- raytracing.rb
+++ raytracing.rb
@@ -73,28 +73,34 @@ class Ray
     origin + (direction * t)
   end
 
-  def color
-    unit_direction = direction.unit_vector
-    a = 0.5 * (unit_direction.y + 1.0)
-    Color.new(1.0, 1.0, 1.0) * (1.0 - a) + (Color.new(0.5, 0.7, 1.0) * a)
+  def hit_sphere(center, radius)
+    # C = center, Q = ray.origin
+    oc = center - origin
+    # a = 2d
+    a = direction.dot(direction)
+    # b = -2d . * (C-Q)
+    b = -2.0 * direction.dot(oc)
+    # c = (C - Q) . (C - Q) - r * r
+    c = oc.dot(oc) - radius * radius
+    discriminant = b * b - 4 * a * c
+
+    if discriminant < 0
+      -1.0
+    else
+      (-b - Math.sqrt(discriminant)) / (2.0 * a)
+    end
   end
-end
 
-def hit_sphere(center, radius, ray)
-  # C = center, Q = ray.origin
-  oc = center - ray.origin
-  # a = 2d
-  a = ray.direction.dot(ray.direction)
-  # b = -2d . * (C-Q)
-  b = -2.0 * ray.direction.dot(oc)
-  # c = (C - Q) . (C - Q) - r * r
-  c = oc.dot(oc) - radius * radius
-  discriminant = b * b - 4 * a * c
-
-  if discriminant < 0
-    -1.0
-  else
-    (-b - Math.sqrt(discriminant)) / (2.0 * a)
+  def color(sphere_center, sphere_radius)
+    hit = hit_sphere(sphere_center, sphere_radius)
+    if hit > 0.0
+      surface_normal = (at(hit) - sphere_center).unit_vector
+      Color.new(surface_normal.x + 1, surface_normal.y + 1, surface_normal.z + 1) * 0.5
+    else
+      unit_direction = direction.unit_vector
+      a = 0.5 * (unit_direction.y + 1.0)
+      Color.new(1.0, 1.0, 1.0) * (1.0 - a) + (Color.new(0.5, 0.7, 1.0) * a)
+    end
   end
 end
 
@@ -118,6 +124,7 @@ def main(image_width: 1600)
 
   puts "P3\n#{image_width} #{image_height}\n255"
   sphere_center = Vector.new(0, 0, -1)
+  sphere_radius = 0.5
 
   (0...image_height).each do |j|
     STDERR.print("\rScanlines remaining: #{image_height - j}")
@@ -126,14 +133,7 @@ def main(image_width: 1600)
       pixel_center = pixel00_loc + (pixel_delta_u * i) + (pixel_delta_v * j)
       ray_direction = pixel_center - camera_center
       ray = Ray.new(camera_center, ray_direction)
-      hit = hit_sphere(sphere_center, 0.5, ray)
-
-      if hit > 0.0
-        surface_normal = (ray.at(hit) - sphere_center).unit_vector
-        puts(Color.new(surface_normal.x + 1, surface_normal.y + 1, surface_normal.z + 1) * 0.5)
-      else
-        puts ray.color
-      end
+      puts ray.color(sphere_center, sphere_radius)
     end
   end
 end
